afa91d40ac3ea78d38c928fa15d2dc6d96060331
Nick Antonizick <Nick@Antonizick.com>
2022-03-20 19:03:47 -0700

    removed cruft files and pushed prototype code

--- code/ct/inc.apps.php
+++
@@ -1,9 +0,0 @@
-<a href="http://www.google.com/a/antonizick.com">Dashboard</a><br><br>
- 
-<a href="http://sites.google.com/a/antonizick.com">Sites</a><br>
-<a href="http://docs.google.com/a/antonizick.com">Docs</a><br>
-<a href="http://calendar.google.com/a/antonizick.com">Calendar</a><br>
-<a href=" http://www.google.com/talk">Talk</a><br>
-<hr>
-		
-</div>
